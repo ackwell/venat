f7fcfa2238fe37a35cfacf1389e19db0b76edd96
ackwell <saxon@ackwell.com.au>
2022-06-13 16:22:55 +1000

    Set up initial auth flow

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,11 +1,32 @@
-import { Outlet, Route, Routes } from 'react-router-dom';
+import {
+  Location,
+  Navigate,
+  Outlet,
+  Route,
+  Routes,
+  useLocation,
+} from 'react-router-dom';
+import { useAtom, useAtomValue } from 'jotai';
+import { atomWithStorage } from 'jotai/utils';
+
+const PERSISTENCE_PREFIX = 'venat:';
+
+// NOTE: this should be updated with finalised API types &c.
+type AuthToken = string;
+const authTokenAtom = atomWithStorage<AuthToken | undefined>(
+  PERSISTENCE_PREFIX + 'authToken',
+  undefined,
+);
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="guilds" element={<GuildList />} />
+        <Route path="auth" element={<Auth />} />
+        <Route element={<RequiresAuth />}>
+          <Route path="guilds" element={<GuildList />} />
+        </Route>
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
@@ -43,7 +64,38 @@ function Home() {
   return <>home</>;
 }
 
-// TODO: this will need some level of auth
+function Auth() {
+  const location = useLocation();
+  const from = (location.state as { from?: Location } | undefined)?.from;
+
+  const [authToken, setAuthToken] = useAtom(authTokenAtom);
+  // TODO: Actual auth flow. Use a callback if the UI becomes non-trivial.
+  const authenticate = () => {
+    setAuthToken('FAKEAUTHENTICATION');
+  };
+
+  return authToken != null ? (
+    <Navigate to={from ?? '/'} replace />
+  ) : (
+    // TODO: move into a seperate UI component if non-trivial
+    <>
+      <h1>auth</h1>
+      <button onClick={authenticate}>press to authenticate</button>
+    </>
+  );
+}
+
+function RequiresAuth() {
+  const authToken = useAtomValue(authTokenAtom);
+  const location = useLocation();
+
+  return authToken == null ? (
+    <Navigate to="/auth" state={{ from: location }} replace />
+  ) : (
+    <Outlet />
+  );
+}
+
 function GuildList() {
   return (
     <ul>
