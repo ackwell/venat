ddf8fa70876e17c134e0d7c39dd8cb99f17a4185
ackwell <saxon@ackwell.com.au>
2022-06-13 15:42:56 +1000

    Set up initial naive router structure

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,3 +1,72 @@
+import { Outlet, Route, Routes } from 'react-router-dom';
+
 export function App() {
-  return <div>content</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="guilds" element={<GuildList />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      header
+      <nav>
+        <ul>
+          <li>link</li>
+          <li>link</li>
+          <li>link</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+// TODO: should authed users be nav'd to guilds instead of landing on home?
+function Home() {
+  return <>home</>;
+}
+
+// TODO: this will need some level of auth
+function GuildList() {
+  return (
+    <ul>
+      <li>
+        <Guild />
+      </li>
+      <li>
+        <Guild />
+      </li>
+      <li>
+        <Guild />
+      </li>
+      <li>
+        <Guild />
+      </li>
+    </ul>
+  );
+}
+
+function Guild() {
+  return <>guild</>;
+}
+
+function NotFound() {
+  return <>page not found</>;
 }
